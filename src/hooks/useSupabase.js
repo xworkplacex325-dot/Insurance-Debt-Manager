44f202da29e922a6cbb54207215022f7985017c9
[Ahmed <ahmed.king332333@gmail.com>
2026-06-24 01:57:42 +0300

    change

--- src/hooks/useSupabase.js
+++ src/hooks/useSupabase.js
@@ -1,4 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://mkiblobbfynexxdrywwn.supabase.co";
-const supabaseKey = "sb_publishable_H4YZMRueGfEUpd9yzSH2Pg_qxgFOlpy";
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
